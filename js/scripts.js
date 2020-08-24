c789c3ff0ac93fb3886cfea043bd5702302425d6
Chris Ramer <chrisramer.97@gmail.com>
2020-08-24 15:55:38 -0700

    Return random game to replay

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,15 @@
 $(document).ready(function () {
 	
-	
+	$("form#favoriteThings").submit(function (e) { 
+		e.preventDefault();
+		
+		const game1 = $("#game1").val();
+		const game2 = $("#game2").val();
+		const game3 = $("#game3").val();
+		const games = [game1, game2, game3];
+
+		const randNum = Math.round(Math.random() * ((games.length - 1)));
+		console.log("I think you should replay " + games[randNum]);
+	});
 
 });
