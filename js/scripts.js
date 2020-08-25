8895891a2fa9dbd37aa1b6a9a1f0cf278c27fed3
Chris Ramer <chrisramer.97@gmail.com>
2020-08-25 15:01:21 -0700

    Add output

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 $(document).ready(function () {
 	
+	function GetRandSelection(array) {
+		return array[Math.round(Math.random() * (array.length - 1))];
+	}
+
 	$("form#favoriteThings").submit(function (e) { 
 		e.preventDefault();
 		
@@ -8,8 +12,24 @@ $(document).ready(function () {
 		const game3 = $("#game3").val();
 		const games = [game1, game2, game3];
 
-		const randNum = Math.round(Math.random() * ((games.length - 1)));
-		console.log("I think you should replay " + games[randNum]);
+		const movie1 = $("#movie1").val();
+		const movie2 = $("#movie2").val();
+		const movie3 = $("#movie3").val();
+		const movies = [movie1, movie2, movie3];
+
+		const band1 = $("#band1").val();
+		const band2 = $("#band2").val();
+		const band3 = $("#band3").val();
+		const bands = [band1, band2, band3];
+
+		const randGame = GetRandSelection(games);
+		const randMovie = GetRandSelection(movies);
+		const randBand = GetRandSelection(bands);
+
+		$("#randGame").text(randGame);
+		$("#randMovie").text(randMovie);
+		$("#randBand").text(randBand);
+		$("#output").show();
 	});
 
 });
